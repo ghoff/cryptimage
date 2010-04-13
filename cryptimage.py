ff9d02625a3adfc63f8ca062fe3d9e56941b8af9
Geoffrey Hoff <geoff.hoff@gmail.com>
2010-04-12 22:34:46 -0400

    Compressed EC public keys and moved code to utility library.
    Created utility functions to cryptimage.py and updated encode and decode to
    use binary strings on stdout and stdin respectively.  Compressing EC points in
    ephemeral public key to save 32 bytes in message.  Still considering move to
    static AES key.

--- cryptimage.py
+++ cryptimage.py
@@ -1,6 +1,49 @@
 from M2Crypto import EVP
 import struct, binascii
 
+_encrypt=1
+_decrypt=0
+
+def parse_input(data):
+	version = ord(data[0])
+	keylen = ord(data[1])
+	key = data[2:keylen+2]
+	cryptlen = ord(data[keylen+2])
+	crypt = data[keylen+3:keylen+3+cryptlen]
+	return (key, crypt)
+
+def encrypt_data(key, data):
+	cipher = EVP.Cipher('aes_128_ecb', key, '', _encrypt)
+	cipher.update(data)
+	result = cipher.final()
+	return(result)
+
+
+def decrypt_data(key, data):
+	cipher = EVP.Cipher('aes_128_ecb', key, '', _decrypt)
+	cipher.update(data)
+	result = cipher.final()
+	return(result)
+
+
+def compress_key(key):
+	if ord(key[0]) != 4:
+		raise Exception, "Invalid key"
+	if ord(key[-1]) % 2 == 0:
+		key = chr(2) + key[1:33]
+	else:
+		key = chr(3) + key[1:33]
+	return(key)
+
+
+def strip_asn1(der):
+	# 2a8648ce3d0201 is OID 1.2.840.10045.2.1 ecPublicKey
+	if der[6:13] != binascii.a2b_hex('2a8648ce3d0201'):
+		raise Exception, "Invalid ASN.1"
+	# strip all but public key
+	return(der[26:])
+
+
 def KDF(key, oBits, P):
 	ZB = key
 	P = binascii.a2b_hex('082A8648CE3D03010722010800416e6f6e796d6f757353656e646572')+P
--- decode.py
+++ decode.py
@@ -2,27 +2,32 @@ from M2Crypto import EC, EVP
 import binascii, sys, cryptimage
 debug=0
 
+data = sys.stdin.read()
+pubkey, ciphertext = cryptimage.parse_input(data)
+
+if debug: sys.stderr.write("ct = %s\n" % binascii.b2a_hex(ciphertext))
+
 ecpair = EC.load_key('eccpair.pem')
 
-asn1header = '3059301306072a8648ce3d020106082a8648ce3d030107034200'
-ephemeral = sys.argv[1][:130]
-data = sys.argv[1][130:]
-ephemeral = EC.pub_key_from_der(binascii.a2b_hex(asn1header + ephemeral))
+#if len(sys.argv[1]) < 150:
+#asn1header = '3059301306072a8648ce3d020106082a8648ce3d030107034200'
+asn1header = '3039301306072a8648ce3d020106082a8648ce3d030107032200'
+#ephemeral = sys.argv[1][:130]
+#data = sys.argv[1][130:]
+#ephemeral = sys.argv[1][:66]
+#data = sys.argv[1][66:]
+ephemeral = EC.pub_key_from_der(binascii.a2b_hex(asn1header) + pubkey)
 
 shared = ecpair.compute_dh_key(ephemeral.pub())
 
 digest = EVP.MessageDigest("sha1")
-digest.update(ecpair.pub().get_der())
+ecder=ecpair.pub().get_der()
+digest.update(ecder)
 fingerprint = digest.digest()
 
-if debug: print "shared key = %s" % binascii.b2a_hex(shared)
 dk=cryptimage.KDF(shared[:len(shared)/2],128,fingerprint)
-if debug: print "derived key = %s" % binascii.b2a_hex(dk)
 
-dec=0
-cipher = EVP.Cipher('aes_128_ecb', dk, '', dec)
-cipher.update(binascii.a2b_hex(data))
-result = cipher.final()
-print "decrypted text = %s" % result
+cleartext = cryptimage.decrypt_data(dk,ciphertext)
+print "decrypted text = %s" % cleartext
 
 sys.exit(0)
